aab6ae7e13a563901a8fe2d95cb298d1872da531
Joel Semar <joel.semar@appiction.com>
2011-04-27 19:41:26 -0500

    fix urls

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
-from webservice_tools.apps.user import import urls as user_urls
+from webservice_tools.apps.user import urls as user_urls
 from webservice_tools.apps.user.handlers import LoginHandler 
 from webserice_tools import urls as service_urls
-from btcserver.handlers import import UserHandler
+from btcserver.handlers import  UserHandler
 from webservice_tools.utils import Resource
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
