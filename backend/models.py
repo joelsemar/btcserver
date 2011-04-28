694b371a3997042e3f8b1b4e80825534aefa3f39
Joel Semar <joel.semar@appiction.com>
2011-04-27 21:04:16 -0500

    stuff

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,41 @@
+import consts
 from django.db import models
-
+from webservice_tools.apps.user.models import BaseProfile
+from backend import bitcoinrpc
 # Create your models here.
+class Table(models.Model):
+    public = models.BooleanField(default=True)
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+    
+    class Meta:
+        abstract = True
+
+class Player(BaseProfile):
+    show_animations = models.BooleanField(default=True)
+    payout_address = models.CharField(max_length=64)
+    username = models.CharField(unique=True, max_length=512)
+    
+    
+    def save(self, *args, **kwargs):
+        if self.user.username != self.username:
+            self.username = self.user.username
+        super(Player, self).save(*args, **kwargs)
+        
+        
+    def create_callback(self):
+        bitcoinrpc.create_account(self.user.username)
+        
+    
+    @property    
+    def balance(self):
+        return bitcoinrpc.get_balance(self.username)
+    
+    @property
+    def address(self):
+        return bitcoinrpc.get_address(self.username)
+    
+    
+class Card(models.Model):
+    name = models.CharField(choices=consts.CARD_CHOICES)
+    value = models.PositiveIntegerField()
--- btcblackjack/models.py
+++ btcblackjack/models.py
@@ -2,10 +2,8 @@ import consts
 from django.db import models
 from webservice_tools.apps.user.models import BaseProfile
 from backend import bitcoinrpc
-class Table(models.Model):
-    public = models.BooleanField(default=True)
-    name = models.CharField(max_length=32)
-    description = models.TextField()
+from backend.models import Card, Player, Table
+class BlackJackTable(Table):
     low_bet = models.DecimalField(max_digits=8, decimal_places=2)
     high_bet = models.DecimalField(max_digits=8, decimal_places=2)
     
@@ -21,14 +19,12 @@ class Table(models.Model):
 class Seat(models.Model):
     position = models.PositiveIntegerField()
     table = models.ForeignKey(Table)
+    player = models.ForeignKey(Player, null=True)
     
-    
-class Card(models.Model):
-    name = models.CharField(choices=consts.CARD_CHOICES)
-    value = models.PositiveIntegerField()
     
 class Hand(models.Model):
     cards = models.ManyToManyField(Card, symmetrical=False)
+    type = models.CharField(max_length=16, choices=consts.HAND_TYPE_CHOICES)
     
     @property
     def score(self):
@@ -37,39 +33,15 @@ class Hand(models.Model):
     
 class Round(models.Model):
     table = models.ForeignKey(Table)
-    dealers_hand = models.ForeignKey(Hand)
+    dealers_hand = models.ForeignKey(BlackJackHand)
 
 
-class Player(BaseProfile):
-    show_animations = models.BooleanField(default=True)
-    payout_address = models.CharField(max_length=64)
-    seat = models.ForeignKey(Seat, null=True)
-    username = models.CharField(unique=True, max_length=512)
-    
-    
-    def save(self, *args, **kwargs):
-        if self.user.username != self.username:
-            self.username = self.user.username
-        super(Player, self).save(*args, **kwargs)
-        
-        
-    def create_callback(self):
-        bitcoinrpc.create_account(self.user.username)
-        
-    
-    @property    
-    def balance(self):
-        return bitcoinrpc.get_balance(self.username)
-    
-    @property
-    def address(self):
-        return bitcoinrpc.get_address(self.username)
     
     
 class Bet(models.Model):
     game = models.ForeignKey(Round)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
-    players_hand = models.ForeignKey(Hand)
+    players_hand = models.ForeignKey(BlackJackHand)
     score = models.PositiveIntegerField()
     player = models.ForeignKey(Player)
         
