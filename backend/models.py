56b46129e536287ff0a7a76a340ddd49d4eabbe3
Joel Semar <joel.semar@appiction.com>
2011-04-28 20:22:43 -0500

    ..

--- backend/models.py
+++ backend/models.py
@@ -14,26 +14,26 @@ class Table(models.Model):
 class Player(BaseProfile):
     show_animations = models.BooleanField(default=True)
     payout_address = models.CharField(max_length=64, default='', blank=True)
-    username = models.CharField(unique=True, max_length=512, blank=True)
+    account_name = models.CharField(unique=True, max_length=512, default='', blank=True)
     
     
     def save(self, *args, **kwargs):
         if self.user.username != self.username:
-            self.username = self.user.username
+            self.account_name = self.user.username
         super(Player, self).save(*args, **kwargs)
         
         
     def create_callback(self):
-        bitcoinrpc.create_account(self.user.username)
+        bitcoinrpc.create_account(self.user.account_name)
         
     
     @property    
     def balance(self):
-        return bitcoinrpc.get_balance(self.username)
+        return bitcoinrpc.get_balance(self.account_name)
     
     @property
     def address(self):
-        return bitcoinrpc.get_address(self.username)
+        return bitcoinrpc.get_address(self.account_name)
     
     
 class Card(models.Model):
