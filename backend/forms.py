c8331b84bad04f51f79361b81cffb36b852063fb
Joel Semar <joel.semar@appiction.com>
2011-04-28 20:33:22 -0500

    migrations

--- backend/forms.py
+++ backend/forms.py
@@ -5,5 +5,8 @@ from webservice_tools.apps.user.forms import ExtModelForm, BaseUserForm
 class UserForm(BaseUserForm):
     pass
 class UserProfileForm(ExtModelForm):
+    
     class Meta:
         model = Player
+        exclude = ('user')
+    
