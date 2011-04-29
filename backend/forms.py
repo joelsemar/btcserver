4f477c29b1953d9da84e3455bdfa579e869cf428
Joel Semar <joel.semar@appiction.com>
2011-04-28 20:30:18 -0500

    migrations

--- backend/forms.py
+++ backend/forms.py
@@ -3,14 +3,6 @@ from django import forms
 from webservice_tools.apps.user.forms import ExtModelForm, BaseUserForm
 
 class UserForm(BaseUserForm):
-    def clean(self):
-        email = self.cleaned_data.get('email')
-        self.cleaned_data['username'] = email
-        return self.cleaned_data
-
+    pass
 class UserProfileForm(ExtModelForm):
-    
-    class Meta:
-        model = Player
-        exclude = ('user', 'friends')
-    
+    pass    
