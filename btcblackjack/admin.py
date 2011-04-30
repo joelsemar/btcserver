5144b517a30fbd18792ba7f1db7afa242f3bdd46
Joel Semar <joel.semar@appiction.com>
2011-04-29 20:39:43 -0500

    admin

---
+++ btcblackjack/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.db.models.base import ModelBase
+from btcblackjack import models
+
+for model_name in dir(models):
+    m = getattr(models, model_name)
+    if isinstance(m, ModelBase):
+        try:
+            admin.site.register(m)
+        except admin.sites.AlreadyRegistered:
+            pass
