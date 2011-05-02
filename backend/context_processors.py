46cf02b8ba63fbb6bd543c264629f45099aeead7
Joel Semar <joel.semar@appiction.com>
2011-05-02 03:01:44 -0500

    seems to be working

--- backend/context_processors.py
+++ backend/context_processors.py
@@ -1,8 +1,9 @@
 
 
 def base(request):
-    player= request.user.get_profile()
     context = {'domain': '127.0.0.1'}
-    context['name'] = player.name 
-    context['balance'] = player.balance
+    if request.user.is_authenticated():
+        player= request.user.get_profile()
+        context['name'] = player.name 
+        context['balance'] = player.balance
     return context
