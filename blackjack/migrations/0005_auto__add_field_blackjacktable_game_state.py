814799756b9aac014608ff349ad72a141b07b549
Joel Semar <joel.semar@appiction.com>
2011-05-06 00:28:54 -0500

    lot of bugs gone, players can see each other

---
+++ blackjack/migrations/0005_auto__add_field_blackjacktable_game_state.py
@@ -0,0 +1,115 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'BlackJackTable.game_state'
+        db.add_column('blackjack_blackjacktable', 'game_state', self.gf('django.db.models.fields.CharField')(default='bidding', max_length=32), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'BlackJackTable.game_state'
+        db.delete_column('blackjack_blackjacktable', 'game_state')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'backend.player': {
+            'Meta': {'object_name': 'Player'},
+            'account_name': ('django.db.models.fields.CharField', [], {'default': "''", 'unique': 'True', 'max_length': '512', 'blank': 'True'}),
+            'handle': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '32', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'payout_address': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'show_animations': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'backend.seat': {
+            'Meta': {'ordering': "('position',)", 'unique_together': "(('table', 'player'),)", 'object_name': 'Seat'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'player': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['backend.Player']", 'null': 'True'}),
+            'position': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'table': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['backend.Table']"})
+        },
+        'backend.table': {
+            'Meta': {'object_name': 'Table'},
+            'current_turn': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'playing_at'", 'null': 'True', 'to': "orm['backend.Seat']"}),
+            'deck': ('django.db.models.fields.TextField', [], {'default': "'[]'"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        'blackjack.blackjackhand': {
+            'Meta': {'object_name': 'BlackJackHand'},
+            'bet': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '12', 'decimal_places': '8'}),
+            'cards': ('django.db.models.fields.CharField', [], {'default': "'[]'", 'max_length': '128'}),
+            'dealers_hand': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'player': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['backend.Player']", 'null': 'True'}),
+            'resolved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'round': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blackjack.BlackJackRound']"})
+        },
+        'blackjack.blackjackround': {
+            'Meta': {'ordering': "('-started',)", 'object_name': 'BlackJackRound'},
+            'closed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'started': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.utcnow'}),
+            'table': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blackjack.BlackJackTable']"}),
+            'taking_bets': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        'blackjack.blackjacktable': {
+            'Meta': {'object_name': 'BlackJackTable', '_ormbases': ['backend.Table']},
+            'game_state': ('django.db.models.fields.CharField', [], {'default': "'bidding'", 'max_length': '32'}),
+            'table_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['backend.Table']", 'unique': 'True', 'primary_key': 'True'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blackjack.BlackJackTableType']"})
+        },
+        'blackjack.blackjacktabletype': {
+            'Meta': {'object_name': 'BlackJackTableType'},
+            'high_bet': ('django.db.models.fields.DecimalField', [], {'max_digits': '12', 'decimal_places': '8'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'low_bet': ('django.db.models.fields.DecimalField', [], {'max_digits': '12', 'decimal_places': '8'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['blackjack']
